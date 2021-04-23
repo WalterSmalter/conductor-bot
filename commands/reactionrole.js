7695230756bbabf4ce712c424f1bee5ac6bfcf14
WalterSmalter <loveofpotatos21@gmail.com>
2021-04-23 18:01:41 +0300

    Relocated reaction listener to events folder

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -11,8 +11,6 @@ module.exports = {
 
         var emojiArray = emojiObject.emojis.split(",");
 
-        const channel = message.channel.id;
-
         var embed = new MessageEmbed()
             .setTitle('Role Selection')
             .setDescription('Choose of the following roles:\n\n**If you react to too many roles at once it will not respond temporarily, give it a minute between each pick!**')
@@ -29,115 +27,11 @@ module.exports = {
             .addField("<:ChillingCello:819558852906188809> - Chilling Cello", "For Cello players")
             .addField("<:VioletViolins:819558737328078848> - Violet Violins", "For Violin players")
             .addField("<:OverachievingOrgs:819559077919064105> - Overachieving Orgs", "For Org players");
-
         let messageEmbed = await message.channel.send(embed);
 
         for(const emoji of emojiArray){
             messageEmbed.react(emoji);
         };
-        
-        client.on('messageReactionAdd', async(reaction, user) =>{
-            if(reaction.message.partial) await reaction.message.fetch();
-            if(reaction.partial) await reaction.fetch();
-            if(user.bot) return;
-            if(!reaction.message.guild) return;
-            
-            if(reaction.message.channel.id == channel) {
-                switch(reaction.emoji.name){
-                    case "sexysaz": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === 'Sexy Saz'));
-                        break;
-                    case "DazzlingDJs": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Dazzling DJs"));
-                        break;
-                    case "DamnDarbukaDrumsDaff": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Damn Darbuka/Drums/Daff"));
-                        break;
-                    case "NocturnalNays": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Nocturnal Nays"));
-                        break;
-                    case "BouzoukBois": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Bouzouk Bois"));
-                        break;
-                    case "FlyingFlute": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Flying Flute"));
-                        break;
-                    case "GuitarGang": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Guitar Gang"));
-                        break;
-                    case "KoolKanoun": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Kool Kanoun"));
-                        break;
-                    case "SingersFromtheSoul": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Singers From the Soul"));
-                        break;
-                    case "Oudsuperior": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Oud (superior)"));
-                        break;
-                    case "ChillingCello": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Chilling Cello"));
-                        break;
-                    case "VioletViolins": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name == 'Violet Violins'));
-                        break;
-                    case "OverachievingOrgs": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(message.guild.roles.cache.find(role => role.name === "Overachieving Orgs"));
-                        break;
-                }
-            }else return;
-        })
-
-        client.on('messageReactionRemove', async(reaction, user) =>{
-            if(reaction.message.partial) await reaction.message.fetch();
-            if(reaction.partial) await reaction.fetch();
-            if(user.bot) return;
-            if(!reaction.message.guild) return;
-            
-            if(reaction.message.channel.id == channel) {
-                switch(reaction.emoji.name){
-                    case "sexysaz": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === 'Sexy Saz'));
-                        break;
-                    case "DazzlingDJs": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Dazzling DJs"));
-                        break;
-                    case "DamnDarbukaDrumsDaff": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Damn Darbuka/Drums/Daff"));
-                        break;
-                    case "NocturnalNays": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Nocturnal Nays"));
-                        break;
-                    case "BouzoukBois": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Bouzouk Bois"));
-                        break;
-                    case "FlyingFlute": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Flying Flute"));
-                        break;
-                    case "GuitarGang": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Guitar Gang"));
-                        break;
-                    case "KoolKanoun": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Kool Kanoun"));
-                        break;
-                    case "SingersFromtheSoul": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Singers From the Soul"));
-                        break;
-                    case "Oudsuperior": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Oud (superior)"));
-                        break;
-                    case "ChillingCello": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Chilling Cello"));
-                        break;
-                    case "VioletViolins": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name == 'Violet Violins'));
-                        break;
-                    case "OverachievingOrgs": 
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Overachieving Orgs"));
-                        break;
-                }
-            }else return;
-        })
-        if(message.channel.id == '819327940953243648') return message.channel.send('**Scroll up to view the rules**');
     }
 
 }
---
+++ events/guild/messageReactionRemove.js
@@ -0,0 +1,50 @@
+module.exports = async(reaction, user) =>{
+    if(reaction.message.partial) await reaction.message.fetch();
+    if(reaction.partial) await reaction.fetch();
+    if(user.bot) return;
+    if(!reaction.message.guild) return;
+
+    if(reaction.message.channel.id == channel) {
+        switch(reaction.emoji.name){
+            case "sexysaz": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === 'Sexy Saz'));
+                break;
+            case "DazzlingDJs": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Dazzling DJs"));
+                break;
+            case "DamnDarbukaDrumsDaff": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Damn Darbuka/Drums/Daff"));
+                break;
+            case "NocturnalNays": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Nocturnal Nays"));
+                break;
+            case "BouzoukBois": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Bouzouk Bois"));
+                break;
+            case "FlyingFlute": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Flying Flute"));
+                break;
+            case "GuitarGang": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Guitar Gang"));
+                break;
+            case "KoolKanoun": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Kool Kanoun"));
+                break;
+            case "SingersFromtheSoul": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Singers From the Soul"));
+                break;
+            case "Oudsuperior": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Oud (superior)"));
+                break;
+            case "ChillingCello": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Chilling Cello"));
+                break;
+            case "VioletViolins": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name == 'Violet Violins'));
+                break;
+            case "OverachievingOrgs": 
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(message.guild.roles.cache.find(role => role.name === "Overachieving Orgs"));
+                break;
+        }
+    }else return;
+}
